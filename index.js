45df3b810d9c414df47e0ee0a970b0080753df6c
digitaldixit <dixit.developerr@gmail.com>
2022-09-23 12:44:42 -0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require("express");
+require('dotenv').config();
 const app = express();
+const cors = require("cors");
+const morgan = require('morgan');
+const PORT = process.env.PORT || 4000
+// customer
 
-app.get("/", function (req, res) {
-  res.send("WORKING!!!");
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+app.get("/",(req,res)=>{
+    res.json("Welcome to API")
 });
 
-app.listen(process.env.PORT || 5000);
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "GET,PUT,PATCH,DELETE,POST,OPTIONS");
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept, Origin');
+    next();
+});
+app.listen(PORT,()=> console.log(`Server is Listening ${PORT}`))
