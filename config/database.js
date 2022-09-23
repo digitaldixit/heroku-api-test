cd4d03b48640ad2f44ee5b3bf49edc7a6c698354
digitaldixit <dixit.developerr@gmail.com>
2022-09-23 13:19:39 -0700

    test db

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ const {createPool} = require('mysql');
 
 const pool = createPool({
     
-    port:process.env.DB_PORT,
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASS,
-    database:process.env.MYSQL_DB,
+    port:3306,
+    host:"sql6.freemysqlhosting.net",
+    user:"sql6521840",
+    password:"t6qCumpZaS",
+    database:"sql6521840",
     connectionLimit:10
 });
 
